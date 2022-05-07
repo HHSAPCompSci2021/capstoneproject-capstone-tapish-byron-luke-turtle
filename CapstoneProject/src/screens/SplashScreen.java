a7e58a057825066863411b86fc657ed76294baa0
Byron Tam <btam226@student.fuhsd.org>
2022-05-06 20:43:01 -0700

    JavaDocs

--- CapstoneProject/src/screens/SplashScreen.java
+++ CapstoneProject/src/screens/SplashScreen.java
@@ -1,12 +1,17 @@
 package screens;
 
 import core.DrawingSurface;
-
+/**
+ * This class represents the home screen when you start the game. 
+ * @version 5/6/22
+ */
 public class SplashScreen extends Screen {
-
 	DrawingSurface surface;
 	
-	
+	/**
+	 * Creates a splash screen with preset dimensions. 
+	 * @param surface
+	 */
 	public SplashScreen(DrawingSurface surface) {
 		super(900, 700);
 		this.surface = surface;
@@ -16,6 +21,9 @@ public class SplashScreen extends Screen {
 		
 	}
 	
+	/**
+	 * 
+	 */
 	public void draw() {
 		
 	}
