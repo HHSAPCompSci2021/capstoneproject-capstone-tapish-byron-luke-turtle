7c8fe0ed5ba7805a8109f4092337b0907505f929
Tapish Singh <tsingh189@student.fuhsd.org>
2022-05-09 14:32:43 -0700

    setting up structure of RiddleBank constructor

--- CapstoneProject/src/sprites/RiddleBank.java
+++ CapstoneProject/src/sprites/RiddleBank.java
@@ -9,9 +9,83 @@ public class RiddleBank {
 	
 	/**
 	 * Creates the selected riddle. 
+	 * All riddles are pulled from https://parade.com/947956/parade/riddles/
+	 * MLA citation: 
 	 * @param riddleNum the number for the riddle chosen(riddles are numbered)
 	 */
 	public RiddleBank(int riddleNum) {
+		if(riddleNum == 1) {
+			riddle = "";
+			answer = "";
+			hint = "";
+		}
+		
+		else if(riddleNum == 2) {
+			riddle = "";
+			answer = "";
+			hint = "";
+		}
+		
+		else if(riddleNum == 3) {
+			riddle = "";
+			answer = "";
+			hint = "";	
+		}
+		
+		else if(riddleNum == 4) {
+			riddle = "";
+			answer = "";
+			hint = "";
+		}
+		
+		else if(riddleNum == 5) {
+			riddle = "";
+			answer = "";
+			hint = "";	
+		}
+		
+		else if(riddleNum == 6) {
+			riddle = "";
+			answer = "";
+			hint = "";
+		}
+		
+		else if(riddleNum == 7) {
+			riddle = "";
+			answer = "";
+			hint = "";
+		}
+		
+		else if(riddleNum == 8) {
+			riddle = "";
+			answer = "";
+			hint = "";
+		}
+		
+		else if(riddleNum == 9) {
+			riddle = "";
+			answer = "";
+			hint = "";
+		}
+		
+		else if(riddleNum == 10) {
+			riddle = "";
+			answer = "";
+			hint = "";
+		}
+		
+		// catch-all in case if number called DOESN'T correspond to an actual riddle
+		else {
+			riddle = "";
+			answer = "";
+			hint = "";
+		}
+		
 		
 	}
 }
+
+
+
+
+
