97ed07aa0a02455afd8706c5e69774e1f69c9820
Chicken <89165779+LukeY0715@users.noreply.github.com>
2022-05-13 14:27:45 -0700

    sprite movement actual

--- CapstoneProject/src/sprites/Enemy.java
+++ CapstoneProject/src/sprites/Enemy.java
@@ -17,6 +17,15 @@ public class Enemy extends Sprite{
 	public Enemy(int x, int y, int width, int height, int faceDir) {
 		super("src/sprites/enemy.png", x, y, width, height, faceDir);
 		
+		
+	}
+	
+	public void move(int dir) {
+		int speed = 4;
+		if(dir == 0) setY(getY() - speed);
+		if(dir == 1) setX(getX() + speed);
+		if(dir == 2) setY(getY() + speed);
+		if(dir == 3) setX(getX() - speed);
 	}
 	
 }
