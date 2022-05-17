bf5dcec24575e0daaed83911dd453166db354b10
Chicken <89165779+LukeY0715@users.noreply.github.com>
2022-05-17 14:50:17 -0700

    attempt to reverse direction for enemy

--- CapstoneProject/src/sprites/Enemy.java
+++ CapstoneProject/src/sprites/Enemy.java
@@ -39,6 +39,9 @@ public class Enemy extends Sprite{
 		if (direction) {
 			setY(getY() + speed);
 		}
+		else if(super.getX() == 0) {
+			setX(-getX());
+		}
 		else {
 			setX(getX() + speed);
 		}
